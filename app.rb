2352cabafd6ee60bf8d503ddb7471af506ff80e3
Anurag Kaushik <anurag11b12345@gmail.com>
2024-03-02 19:18:09 +0000

    Updated

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ FunctionsFramework.http "app" do |request|
  a = request.params["a"].to_i
  b = request.params["b"].to_i
  calculator = Calculator.new
- add = calculator.add(a, b)
+ add = calculator.add(a,b)
  "Result: #{add}"
 end
 
